35ba386f6c54fbfe5ec5cf619574cb2f1a482a9a
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-03-15 18:55:34 +0200

    Document Miles to km

--- intro-and-basic-syntax/exercise/MilestoKM.cs
+++ intro-and-basic-syntax/exercise/MilestoKM.cs
@@ -6,10 +6,11 @@ namespace MilestoKM
     {
         static void Main(string[] args)
         {
+            //Read number of miles from the Console
             double miles = double.Parse(Console.ReadLine());
-
+            //One mile is equal to 1.60934km
             double km = miles * 1.60934;
-
+            //Print the result formatted to the second digit using f2 or 0.00
             Console.WriteLine($"{km:f2}");
         }
     }
