fc0d295c0f4fbf4c9188f7663e8ac2e0715af7d0
StanZash <zashev_25@abv.bg>
2018-03-15 19:00:45 +0200

    documents first and fourth problem

--- intro-and-basic-syntax/exercise/BeverageLabels.cs
+++ intro-and-basic-syntax/exercise/BeverageLabels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// CONFIRMED from StanZash
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,16 @@ namespace _04.BeverageLabels
     {
         static void Main(string[] args)
         {
+            // Take product's name
             string name = Console.ReadLine();
+            // Take product's parameters
             int volume = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int sugar = int.Parse(Console.ReadLine());
-
+            //Calculate energy and sugar in product
             double energyContent = volume / 100.0 * energy;
             double sugarContent = volume / 100.0 * sugar;
-
+            // Print results
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{energyContent}kcal, {sugarContent}g sugars");
 
--- intro-and-basic-syntax/exercise/DebitCard.cs
+++ intro-and-basic-syntax/exercise/DebitCard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// CONFIRMED from StanZash
+using System;
 
 namespace DebitCard
 {
@@ -6,11 +7,12 @@ namespace DebitCard
     {
         static void Main(string[] args)
         {
+            //Take cards numbers from the console
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
             int num3 = int.Parse(Console.ReadLine());
             int num4 = int.Parse(Console.ReadLine());
-
+            //Print formatted numbers
             Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
         }
     }
