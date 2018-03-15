ef4d37e26d38681e717d26e2800b345d6fe7e663
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-03-15 19:45:18 +0200

    Change variables - 1 and 2 problem

--- intro-and-basic-syntax/exercise/DebitCard.cs
+++ intro-and-basic-syntax/exercise/DebitCard.cs
@@ -8,12 +8,12 @@ namespace DebitCard
         static void Main(string[] args)
         {
             //Take cards numbers from the console
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-            int num3 = int.Parse(Console.ReadLine());
-            int num4 = int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
             //Print formatted numbers
-            Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
+            Console.WriteLine($"{number1:d4} {number2:d4} {number3:d4} {number4:d4}");
         }
     }
 }
--- intro-and-basic-syntax/exercise/RectangleArea.cs
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -11,11 +11,11 @@ namespace rectangular
         static void Main(string[] args)
         {
             // We take the ractangle wigth and height in two lines, as floating point numbers
-            double width = double.Parse(Console.ReadLine());
-            double height = double.Parse(Console.ReadLine());
+            double recWidth = double.Parse(Console.ReadLine());
+            double recHeight = double.Parse(Console.ReadLine());
 
             //We print the resulting ractangle area following the formula width * height
-            Console.WriteLine("{0:0.00}", width * height);
+            Console.WriteLine("{0:0.00}", recWidth * recHeight);
         }
     }
 }
